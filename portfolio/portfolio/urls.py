8c54d8d461ae1ec1067e9b0f5759ca07595de8fa
Anon1929 <anon89345@outlook.com>
2022-06-24 05:20:53 -0300

    updates

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda req: redirect('/projects/')),
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
