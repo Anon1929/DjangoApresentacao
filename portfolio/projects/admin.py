3f65fe62b12ae6740542367da88e006fe5486443
Anon1929 <anon89345@outlook.com>
2022-06-24 04:53:21 -0300

    finished apps

--- portfolio/projects/admin.py
+++ portfolio/projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from projects.models import *
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Project, ProjectAdmin)
 
 # Register your models here.
